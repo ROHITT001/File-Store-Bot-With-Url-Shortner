c2f6ed708ce15c457a85b7b23533090027d557fb
ROHITT001 <157211770+ROHITT001@users.noreply.github.com>
2024-01-21 07:48:27 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,16 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "28421635"))
+  API_HASH = os.environ.get("API_HASH", "a4856de5fec0b9b3601ff06425f3f58e
+  ")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6947074831:AAHfx5XvS6p0BXjamh3mdIKtjaC6MMgM4x0")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "AnimePediaFile_Bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001970343871"))
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6657660775"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Filebot:Filebot@cluster0.1kdt8x3.mongodb.net/?retryWrites=true&w=majority")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001991806323")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002093375923"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -19,24 +18,23 @@ class Config(object):
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
   ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
-Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+Send Me any File Or Video You Want. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
 
-╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
+╭────[ 🍁FɪʟᴇSᴛᴏʀᴇBᴏᴛ🍁]────⍟
 │
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+├➥ My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
 │
-├🔸 Language: [Python 3](https://www.python.org)
+├➥ Language: [Python 3](https://www.python.org)
 │
-├🔹 Library: [Pyrogram](https://docs.pyrogram.org)
+├➥ Library: [Pyrogram](https://docs.pyrogram.org)
 │
-╰──────[ 😎 ]───────────⍟
+╰───────────────────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: (https://t.me/TeamAnimePedia)
  
- I am Super noob Please Support My Hard Work.
-
-[Donate Me](https://t.me/KingVj01)
+ Working Hard To be Perfect 
+ 
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
